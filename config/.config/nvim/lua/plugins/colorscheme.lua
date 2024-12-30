c3d8e7f77eb7ddf396166a3549dea1021180331f
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2024-12-30 03:05:38 +0100

    change: colorscheme to kanagawa and github_light

--- config/.config/nvim/lua/plugins/colorscheme.lua
+++ config/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,13 +4,13 @@ return {
 	},
 	{
 		"rebelot/kanagawa.nvim",
+		priority = 1000, -- Ensure it loads first
+		config = function()
+			vim.cmd("colorscheme kanagawa")
+		end,
 	},
 	{
 		"olimorris/onedarkpro.nvim",
-		priority = 1000, -- Ensure it loads first
-		config = function()
-			vim.cmd("colorscheme onedark")
-		end,
 	},
 	{
 		"f-person/auto-dark-mode.nvim",
@@ -19,11 +19,11 @@ return {
 		opts = {
 			update_interval = 1000,
 			set_dark_mode = function()
-				vim.cmd("colorscheme onedark")
-				-- vim.opt.background = "dark"
+				vim.cmd("colorscheme kanagawa")
+				vim.opt.background = "dark"
 			end,
 			set_light_mode = function()
-				vim.cmd("colorscheme onelight")
+				vim.cmd("colorscheme github_light")
 				-- vim.opt.background = "light"
 			end,
 		},
