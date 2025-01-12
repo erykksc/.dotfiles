f89d111ad9db5950e0d17f07b9ab97cb90f89920
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-01-12 18:30:18 +0100

    add neogit config for nvim and disable fugitive

--- config/.config/nvim/lua/plugins/fugitive.lua
+++ config/.config/nvim/lua/plugins/fugitive.lua
@@ -1,8 +1,8 @@
 return {
-	"tpope/vim-fugitive",
-	keys = {
-		{ "<leader>gs", "<cmd>0G<cr>", desc = "[Git] Fugitive [S]ummary" },
-		{ "<leader>gh", "<cmd>diffget //2<cr>", desc = "Get diff from the left side" },
-		{ "<leader>gl", "<cmd>diffget //3<cr>", desc = "Get diff from the right side" },
-	},
+	-- 	"tpope/vim-fugitive",
+	-- 	keys = {
+	-- 		{ "<leader>gs", "<cmd>0G<cr>", desc = "[Git] Fugitive [S]ummary" },
+	-- 		{ "<leader>gh", "<cmd>diffget //2<cr>", desc = "Get diff from the left side" },
+	-- 		{ "<leader>gl", "<cmd>diffget //3<cr>", desc = "Get diff from the right side" },
+	-- 	},
 }
