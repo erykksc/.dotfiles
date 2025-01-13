17a9eb9ac81de4d555f87d4e07608c5bfb3ba45e
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-01-13 13:17:31 +0100

    change colorscheme to github

--- config/.config/nvim/lua/plugins/colorscheme.lua
+++ config/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,27 @@
 return {
 	{
 		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			style = "moon",
-			transparent = false,
-			terminal_colors = true,
-			styles = {
-				comments = { italic = false },
-				keywords = { italic = false },
-				sidebars = "dark",
-				floats = "dark",
-			},
-		},
-		config = function()
-			vim.cmd("colorscheme tokyonight")
-		end,
+		-- lazy = false,
+		-- priority = 1000,
+		-- opts = {
+		-- 	style = "moon",
+		-- 	transparent = false,
+		-- 	terminal_colors = true,
+		-- 	styles = {
+		-- 		comments = { italic = false },
+		-- 		keywords = { italic = false },
+		-- 		sidebars = "dark",
+		-- 		floats = "dark",
+		-- 	},
+		-- },
+		-- config = function()
+		-- 	vim.cmd("colorscheme tokyonight")
+		-- end,
 	},
 
 	{
 		"projekt0n/github-nvim-theme",
+		priority = 1000,
 	},
 	{
 		"rebelot/kanagawa.nvim",
@@ -35,11 +36,11 @@ return {
 		opts = {
 			update_interval = 1000,
 			set_dark_mode = function()
-				vim.cmd("colorscheme tokyonight-moon")
+				vim.cmd("colorscheme github_dark_default")
 				-- vim.opt.background = "dark"
 			end,
 			set_light_mode = function()
-				vim.cmd("colorscheme tokyonight-day")
+				vim.cmd("colorscheme github_light")
 				-- vim.opt.background = "light"
 			end,
 		},
