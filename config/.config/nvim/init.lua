a707a8bb4a0e77cca7e2e70299e377e510a46d14
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-01-15 16:37:13 +0100

    feat(nvim): add keymaps for executing lua

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -58,6 +58,11 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
+-- Execute lua keymaps
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute current lua file" })
+vim.keymap.set("n", "<leader>x", ":.lua<CR>", { desc = "Execute current lua line" })
+vim.keymap.set("v", "<leader>x", ":lua<CR>", { desc = "Execute selected lua" })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
