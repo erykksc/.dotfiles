a8897dffcde12505467397a43ddd1e785c5da09d
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-01-15 16:31:58 +0100

    feat(nvim): add <M-Tab> for <Plug>(copilot-suggest)

--- config/.config/nvim/lua/plugins/copilot.lua
+++ config/.config/nvim/lua/plugins/copilot.lua
@@ -3,11 +3,7 @@ return {
 		"github/copilot.vim",
 		config = function()
 			vim.cmd(":Copilot disable")
-			-- vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', {
-			-- 	expr = true,
-			-- 	replace_keycodes = false,
-			-- })
-			-- vim.g.copilot_no_tab_map = true
+			vim.keymap.set("i", "<M-Tab>", "<Plug>(copilot-suggest)")
 		end,
 	},
 	{
