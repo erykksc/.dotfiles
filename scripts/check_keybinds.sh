77941a0138ae26cd38b01099998d72bc02d4b91e
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-03-28 18:19:41 +0100

    fix(check_keybinds.sh): wrong paths

--- scripts/check_keybinds.sh
+++ scripts/check_keybinds.sh
@@ -8,7 +8,7 @@ jq '.profiles
 	| select(.description | contains("open") and contains("Meh"))
 	| .manipulators 
 	| .[] 
-	| "\(.from.key_code),\(.to.[0].shell_command)"' ~/.dotfiles/config/.config/karabiner/karabiner.json \
+	| "\(.from.key_code),\(.to.[0].shell_command)"' ~/.config/karabiner/karabiner.json \
 	| sed 's/"//g' \
 	| sed 's/open /,/g' \
 	| sed 's/^/Meh + /' \
@@ -21,7 +21,7 @@ jq '.profiles
 	| select(.description | contains("open") and contains("Hyper"))
 	| .manipulators 
 	| .[] 
-	| "\(.from.key_code),\(.to.[0].shell_command)"' ~/.dotfiles/config/.config/karabiner/karabiner.json \
+	| "\(.from.key_code),\(.to.[0].shell_command)"' ~/.config/karabiner/karabiner.json \
 	| sed 's/"//g' \
 	| sed 's/open /,/g' \
 	| sed 's/^/Hyper + /' \
