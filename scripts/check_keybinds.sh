bdbf0c67f9cb1db792adc3dce8bb20ffd1321b6d
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-03-28 14:02:14 +0100

    feat: add check_keybinds.sh script

---
+++ scripts/check_keybinds.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+
+# Print all hyper key keybinds
+jq '.profiles
+	| .[]
+	| .complex_modifications.rules
+	| .[]
+	| select(.description | contains("open") and contains("Meh"))
+	| .manipulators 
+	| .[] 
+	| "\(.from.key_code),\(.to.[0].shell_command)"' ~/.dotfiles/config/.config/karabiner/karabiner.json \
+	| sed 's/"//g' \
+	| sed 's/open /,/g' \
+	| sed 's/^/Meh + /' \
+	| column -ts ,
+
+jq '.profiles
+	| .[]
+	| .complex_modifications.rules
+	| .[]
+	| select(.description | contains("open") and contains("Hyper"))
+	| .manipulators 
+	| .[] 
+	| "\(.from.key_code),\(.to.[0].shell_command)"' ~/.dotfiles/config/.config/karabiner/karabiner.json \
+	| sed 's/"//g' \
+	| sed 's/open /,/g' \
+	| sed 's/^/Hyper + /' \
+	| column -ts ,
