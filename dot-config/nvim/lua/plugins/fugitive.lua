1273347af0dbdc39681693ca84f24b9a9bc1d602
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-05-30 01:53:33 +0200

    chore(nvim): remove unnecessary plugins

--- dot-config/nvim/lua/plugins/fugitive.lua
+++
@@ -1,8 +0,0 @@
-return {
-	-- 	"tpope/vim-fugitive",
-	-- 	keys = {
-	-- 		{ "<leader>gs", "<cmd>0G<cr>", desc = "[Git] Fugitive [S]ummary" },
-	-- 		{ "<leader>gh", "<cmd>diffget //2<cr>", desc = "Get diff from the left side" },
-	-- 		{ "<leader>gl", "<cmd>diffget //3<cr>", desc = "Get diff from the right side" },
-	-- 	},
-}
