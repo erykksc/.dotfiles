dd93ef7273ecc4df60ab2173af1f9d846e168f6a
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-06-13 18:29:23 +0200

    fix(nvim/copilot): configure for blink.nvim

--- dot-config/nvim/lua/plugins/copilot.lua
+++ dot-config/nvim/lua/plugins/copilot.lua
@@ -1,82 +1,89 @@
 return {
 	{
 		"zbirenbaum/copilot.lua",
-		config = function()
-			require("copilot").setup({
-				panel = {
-					enabled = false,
-				},
-				suggestion = {
-					enabled = true,
-					auto_trigger = false,
-					hide_during_completion = true,
-					debounce = 75,
-					keymap = {
-						accept = "<M-e>",
-						accept_word = false,
-						accept_line = false,
-						next = "<M-]>",
-						prev = "<M-[>",
-						dismiss = "<C-]>",
-					},
-				},
-				filetypes = {
-					yaml = false,
-					markdown = false,
-					help = false,
-					gitcommit = false,
-					gitrebase = false,
-					hgcommit = false,
-					svn = false,
-					cvs = false,
-					["."] = false,
-				},
-				copilot_node_command = "node", -- Node.js version must be > 18.x
-				server_opts_overrides = {},
-			})
-		end,
+		opts = {
+			suggestion = { enabled = false },
+			panel = { enabled = false },
+		},
 	},
 	-- {
-	-- 	"github/copilot.vim",
+	-- 	"zbirenbaum/copilot.lua",
 	-- 	config = function()
-	-- 		vim.cmd(":Copilot disable")
-	-- 		vim.keymap.set("i", "<M-Tab>", "<Plug>(copilot-suggest)")
+	-- 		require("copilot").setup({
+	-- 			panel = {
+	-- 				enabled = false,
+	-- 			},
+	-- 			suggestion = {
+	-- 				enabled = true,
+	-- 				auto_trigger = false,
+	-- 				hide_during_completion = true,
+	-- 				debounce = 75,
+	-- 				keymap = {
+	-- 					accept = "<M-e>",
+	-- 					accept_word = false,
+	-- 					accept_line = false,
+	-- 					next = "<M-]>",
+	-- 					prev = "<M-[>",
+	-- 					dismiss = "<C-]>",
+	-- 				},
+	-- 			},
+	-- 			filetypes = {
+	-- 				yaml = false,
+	-- 				markdown = false,
+	-- 				help = false,
+	-- 				gitcommit = false,
+	-- 				gitrebase = false,
+	-- 				hgcommit = false,
+	-- 				svn = false,
+	-- 				cvs = false,
+	-- 				["."] = false,
+	-- 			},
+	-- 			copilot_node_command = "node", -- Node.js version must be > 18.x
+	-- 			server_opts_overrides = {},
+	-- 		})
 	-- 	end,
 	-- },
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "main",
-		dependencies = {
-			{ "github/copilot.vim" },
-			{ "nvim-lua/plenary.nvim" },
-		},
-		opts = {
-			debug = false,
-			auto_insert_mode = false,
-			show_help = false,
-		},
-		keys = {
-			{
-				"<leader>ab",
-				function()
-					local input = vim.fn.input("Ask about Buffer: ")
-					if input ~= "" then
-						require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
-					end
-				end,
-				mode = { "n", "x" },
-				desc = "[A]sk about [B]uffer",
-			},
-			{ "<leader>at", "<cmd>CopilotChatToggle<CR>", mode = { "n", "x" }, desc = "[A]sk Copilot [T]oggle" },
-			{ "<leader>ar", "<cmd>CopilotChatReview<CR>", mode = { "n", "x" }, desc = "[A]sk for [R]eview" },
-			{ "<leader>au", "<cmd>CopilotChatTests<CR>", mode = { "n", "x" }, desc = "[A]sk for [U]nit tests" },
-			{ "<leader>ae", "<cmd>CopilotChatExplain<CR>", mode = { "n", "x" }, desc = "[A]sk to [E]xplain" },
-			{
-				"<leader>ac",
-				"<cmd>CopilotChatCommit<CR>",
-				mode = { "n", "x" },
-				desc = "[A]sk for [C]ommit Message of Staged",
-			},
-		},
-	},
+	-- -- {
+	-- -- 	"github/copilot.vim",
+	-- -- 	config = function()
+	-- -- 		vim.cmd(":Copilot disable")
+	-- -- 		vim.keymap.set("i", "<M-Tab>", "<Plug>(copilot-suggest)")
+	-- -- 	end,
+	-- -- },
+	-- {
+	-- 	"CopilotC-Nvim/CopilotChat.nvim",
+	-- 	branch = "main",
+	-- 	dependencies = {
+	-- 		{ "github/copilot.vim" },
+	-- 		{ "nvim-lua/plenary.nvim" },
+	-- 	},
+	-- 	opts = {
+	-- 		debug = false,
+	-- 		auto_insert_mode = false,
+	-- 		show_help = false,
+	-- 	},
+	-- 	keys = {
+	-- 		{
+	-- 			"<leader>ab",
+	-- 			function()
+	-- 				local input = vim.fn.input("Ask about Buffer: ")
+	-- 				if input ~= "" then
+	-- 					require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+	-- 				end
+	-- 			end,
+	-- 			mode = { "n", "x" },
+	-- 			desc = "[A]sk about [B]uffer",
+	-- 		},
+	-- 		{ "<leader>at", "<cmd>CopilotChatToggle<CR>", mode = { "n", "x" }, desc = "[A]sk Copilot [T]oggle" },
+	-- 		{ "<leader>ar", "<cmd>CopilotChatReview<CR>", mode = { "n", "x" }, desc = "[A]sk for [R]eview" },
+	-- 		{ "<leader>au", "<cmd>CopilotChatTests<CR>", mode = { "n", "x" }, desc = "[A]sk for [U]nit tests" },
+	-- 		{ "<leader>ae", "<cmd>CopilotChatExplain<CR>", mode = { "n", "x" }, desc = "[A]sk to [E]xplain" },
+	-- 		{
+	-- 			"<leader>ac",
+	-- 			"<cmd>CopilotChatCommit<CR>",
+	-- 			mode = { "n", "x" },
+	-- 			desc = "[A]sk for [C]ommit Message of Staged",
+	-- 		},
+	-- 	},
+	-- },
 }
