c8f5f0e629f739956620ea776a2387a17acd39d7
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-07-14 19:41:47 +0200

    simplify: colorscheme config to dark only

--- dot-config/nvim/lua/plugins/colorscheme.lua
+++ dot-config/nvim/lua/plugins/colorscheme.lua
@@ -4,47 +4,23 @@ return {
 	},
 	{
 		"RRethy/base16-nvim",
+		config = function()
+			vim.cmd.colorscheme("base16-ayu-mirage")
+		end
 	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-	},
-	{
-		"f-person/auto-dark-mode.nvim",
-		opts = {
-			set_dark_mode = function()
-				-- vim.api.nvim_set_option_value("background", "dark", {})
-				vim.cmd.colorscheme("base16-ayu-mirage")
-			end,
-			set_light_mode = function()
-				-- vim.api.nvim_set_option_value("background", "light", {})
-				vim.cmd.colorscheme("base16-ayu-light")
-			end,
-			update_interval = 3000,
-			fallback = "dark",
-		},
-	},
-	{
-		"Shatur/neovim-ayu",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"shaunsingh/nord.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"erykksc/ghostty-theme-sync.nvim",
-		branch = "main",
-		opts = {
-			persist_nvim_theme = true,
-			nvim_config_path = "~/.config/nvim/lua/plugins/colorscheme.lua", -- Add your configuration here
-		},
-	},
+	-- {
+	-- 	"f-person/auto-dark-mode.nvim",
+	-- 	opts = {
+	-- 		set_dark_mode = function()
+	-- 			-- vim.api.nvim_set_option_value("background", "dark", {})
+	-- 			vim.cmd.colorscheme("base16-ayu-mirage")
+	-- 		end,
+	-- 		set_light_mode = function()
+	-- 			-- vim.api.nvim_set_option_value("background", "light", {})
+	-- 			vim.cmd.colorscheme("base16-ayu-light")
+	-- 		end,
+	-- 		update_interval = 3000,
+	-- 		fallback = "dark",
+	-- 	},
+	-- },
 }
