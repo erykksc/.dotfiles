27cbecc7ee45afce42a25242880e4c8919221e7a
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-10-13 15:45:24 +0200

    feat: add dev-nix.sh

---
+++ setup/dev-nix.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+# Install Nix
+if command -v nix >/dev/null 2>&1; then
+	echo "nix already installed"
+else
+	curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate --no-confirm \
+		--extra-conf "tarball-ttl = 1w" # update nixpkgs automatically on nix run only once a week
+	# enable nix
+	. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+fi
+
+nix profile upgrade --all
+
+nix profile add \
+	'nixpkgs#btop' \
+	'nixpkgs#cmake' \
+	'nixpkgs#codex' \
+	'nixpkgs#delta' \
+	'nixpkgs#devcontainer' \
+	'nixpkgs#direnv' \
+	'nixpkgs#fd' \
+	'nixpkgs#fzf' \
+	'nixpkgs#gh' \
+	'nixpkgs#git' \
+	'nixpkgs#git-lfs' \
+	'nixpkgs#go' \
+	'nixpkgs#mosh' \
+	'nixpkgs#nix-direnv' \
+	'nixpkgs#nixfmt-rfc-style' \
+	'nixpkgs#pnpm' \
+	'nixpkgs#prettier' \
+	'nixpkgs#prettierd' \
+	'nixpkgs#python3' \
+	'nixpkgs#ripgrep' \
+	'nixpkgs#tlrc' \
+	'nixpkgs#tmux' \
+	'nixpkgs#tree' \
+	'nixpkgs#uv' \
+	'nixpkgs#watch' \
+	'nixpkgs#watchexec' \
+	'nixpkgs#wget'
