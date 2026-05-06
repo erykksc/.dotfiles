e676f8a4eacdd65f3a3532e89aa64aefdfe42f76
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2026-05-06 07:25:09 +0200

    fzf-terminal: add xdg-open keybind

--- dot-local/bin/fzf-terminal.sh
+++ dot-local/bin/fzf-terminal.sh
@@ -5,12 +5,13 @@ search_paths=(~ ~/dev)
 selected=$(
 	{
 		echo "$HOME" # first entry is home to put it as the first result
+		echo "$HOME/.local/share/applications"
 		find -L "${search_paths[@]}" -mindepth 1 -maxdepth 1 -type d
-		find -L ~/Documents -type d
-		find -L "${search_paths[@]}" -maxdepth 2 -name "*.kitty-session"
-		find -L "$HOME/.config/kitty" -maxdepth 2 -name "*.kitty-session"
-	} | fzf --header "Enter: Open Kitty | Ctrl-E: Open in Neovim" \
-		--bind 'ctrl-e:execute(nvim {})+abort'
+		find ~/Documents \( -name "node_modules" -o -name ".venv" -o -name ".git" \) -prune -o -type d
+		find -L "${search_paths[@]}" -maxdepth 1 -name "*.kitty-session"
+	} | fzf --header "Enter: Open Kitty | Ctrl-E: Open in Neovim | Ctrl-Enter: xdg-open" \
+		--bind 'ctrl-e:execute(nvim {})+abort' \
+		--bind 'ctrl-o:execute(xdg-open {})+abort'
 )
 
 if [[ -z $selected ]]; then
