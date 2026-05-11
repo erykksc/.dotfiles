dfe530b68e160c6617440d6fccebb0fa0bed84fa
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2026-05-11 13:57:51 +0200

    feat: add sync-projects and simplify the kitty-session handling

---
+++ dot-local/bin/sync-projects.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+APP_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/applications"
+
+shopt -s nullglob
+projects=("$HOME"/dev/*/)
+
+desktop-escape() {
+	local value="$1"
+	value=${value//\\/\\\\}
+	value=${value//$'\n'/\\n}
+	value=${value//$'\r'/\\r}
+	value=${value//$'\t'/\\t}
+	printf '%s' "$value"
+}
+
+desktop-exec-arg() {
+	local value="$1"
+	value=${value//\\/\\\\}
+	value=${value//\"/\\\"}
+	value=${value//\`/\\\`}
+	value=${value//\$/\\\$}
+	value=${value//%/%%}
+	printf '"%s"' "$value"
+}
+
+desktop-content() {
+	local projectPath="$1"
+	local projectName
+	local definedSession
+	projectName=$(basename "$projectPath")
+
+	definedSession=$(find "$HOME/.dotfiles/dot-config/kitty/sessions/" -type f -name "$projectName.kitty-session" -print -quit)
+
+	if [ -z "$definedSession" ]; then
+		definedSession="$HOME/.dotfiles/dot-config/kitty/sessions/generic.kitty-session"
+	fi
+
+	printf '%s\n' \
+		'[Desktop Entry]' \
+		'Type=Application' \
+		"Name=$(desktop-escape "$projectName")" \
+		"Exec=kitty --detach --directory $(desktop-exec-arg "$projectPath") --session $(desktop-exec-arg "$definedSession")" \
+		'Icon=kitty' \
+		'Terminal=false' \
+		'Categories=System;TerminalEmulator;' \
+		'TryExec=kitty' \
+		'X-Kitty-Session-Managed=true' \
+		"X-Kitty-Session-Path=$(desktop-escape "$definedSession")"
+}
+
+echo "Deleting old auto kitty sessions"
+mkdir -p "$APP_DIR"
+find "$APP_DIR" -type f -name "auto-kitty-session*" -delete
+
+for project in "${projects[@]}"; do
+	pname=$(basename "$project")
+	desktopFilepath="$APP_DIR/auto-kitty-session_$pname.desktop"
+	desktop-content "$project" >"$desktopFilepath"
+	echo "Created $desktopFilepath"
+done
+
+# force update of desktop entries by DE
+update-desktop-database ~/.local/share/applications
