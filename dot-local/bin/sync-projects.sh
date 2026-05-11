5332f1926daf51fdf37849fef7cd86b4ccb95dc4
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2026-05-11 19:37:14 +0200

    sync-projects.sh: remove project class name and add title

--- dot-local/bin/sync-projects.sh
+++ dot-local/bin/sync-projects.sh
@@ -27,10 +27,8 @@ desktop-exec-arg() {
 desktop-content() {
 	local projectPath="$1"
 	local projectName
-	local appId
 	local definedSession
 	projectName=$(basename "$projectPath")
-	appId="auto-kitty-session_$projectName"
 
 	definedSession=$(find "$HOME/.dotfiles/dot-config/kitty/sessions/" -type f -name "$projectName.kitty-session" -print -quit)
 
@@ -42,11 +40,10 @@ desktop-content() {
 		'[Desktop Entry]' \
 		'Type=Application' \
 		"Name=$(desktop-escape "$projectName")" \
-		"Exec=kitty --detach --class $(desktop-exec-arg "$appId") --directory $(desktop-exec-arg "$projectPath") --session $(desktop-exec-arg "$definedSession")" \
+		"Exec=kitty --detach --title $(desktop-exec-arg "$projectName") --directory $(desktop-exec-arg "$projectPath") --session $(desktop-exec-arg "$definedSession")" \
 		'Icon=kitty' \
 		'Terminal=false' \
 		'StartupNotify=true' \
-		"StartupWMClass=$(desktop-escape "$appId")" \
 		'Categories=System;TerminalEmulator;' \
 		'TryExec=kitty' \
 		'X-Kitty-Session-Managed=true' \
